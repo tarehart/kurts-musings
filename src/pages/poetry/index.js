cada7e9f9b15ad788a6231a26e8f3be762c8deb0
Tyler Arehart <tarehart@gmail.com>
2023-05-24 08:58:19 -0700

    Subheading on the poetry page.

--- src/pages/poetry/index.js
+++ src/pages/poetry/index.js
@@ -2,26 +2,18 @@ import * as React from "react";
 
 import Layout from "../../components/Layout";
 import PoetryRoll from "../../components/PoetryRoll";
+import FullWidthImage from "../../components/FullWidthImage";
 
 export default class PoetryIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-          backgroundImage: `url('/img/_DSC2862-Essays Print Master.jpg')`,
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1 site-title"
-            style={{
-              padding: "1rem",
-            }}
-          >
-            Poetry of Helen W. Arehart
-          </h1>
-        </div>
+        <FullWidthImage 
+          img="/img/_DSC2862-Essays Print Master.jpg"
+          height={280}
+          title="Poetry of Helen W. Arehart"
+          subheading="Favorites From My Mom's Pencil.  They Wrote Themselves, She Claimed."
+        />
         <section className="section">
           <div className="container">
             <div className="content">
