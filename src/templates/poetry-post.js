a84cc2a0fd5420614906d71e2fd44f6ea1cc98ec
Tyler Arehart <tarehart@gmail.com>
2023-05-24 20:17:19 -0700

    Removing cruft on poetry page that was causing horizontal scroll.

--- src/templates/poetry-post.js
+++ src/templates/poetry-post.js
@@ -21,26 +21,22 @@ export const PoetryPostTemplate = ({
     <section className="section poetry">
       {helmet || ""}
       <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+        <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+          {title}
+        </h1>
+        <PostContent content={content} />
+        {tags && tags.length ? (
+          <div style={{ marginTop: `4rem` }}>
+            <h4>Tags</h4>
+            <ul className="taglist">
+              {tags.map((tag) => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
+        ) : null}
       </div>
       {/* SASS seems to be removing unused styles that it can't see in the PostContent, so trick it. */}
       <div style={{display: 'none'}}>
